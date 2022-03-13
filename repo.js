3d55a26cbf98866284cb6860471102bc9bdc343d
KabanFriends <yuukun2004@protonmail.ch>
2022-03-14 00:50:43 +0900

    Update repo

--- repo.js
+++ repo.js
@@ -1,8 +1,11 @@
 {
-    "id": "thgte",
-    "title": "Project 6 (a.k.a. Touhou Project: Google Translate Edition)",
-    "contact": "KabanFriends#7721 on Discord",
-    "patches": {
-        "th10gte": "Strong East Wind - Rock of Faith (Touhou 10: Google Translate Edition)"
-    }
+	"contact": "KabanFriends#7721 on Discord",
+	"id": "thgte",
+	"patches": {
+		"th10gte": "Touhou 10: Google Translate Edition"
+	},
+	"servers": [
+		"https://github.com/KabanFriends/THGTE"
+	],
+	"title": "Project 6 (a.k.a. Touhou Project: Google Translate Edition)"
 }
--- th10gte/patch.js
+++ th10gte/patch.js
@@ -1,8 +1,11 @@
 {
-  "dependencies": [
-    "nmlgc/base_tsa",
-    "nmlgc/base_tasofro"
-  ],
-  "id": "th10gte",
-  "title": "Touhou 10: Google Translate Edition"
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "th10gte",
+	"servers": [
+		"https://github.com/KabanFriends/THGTE/th10gte/"
+	],
+	"title": "Touhou 10: Google Translate Edition"
 }
